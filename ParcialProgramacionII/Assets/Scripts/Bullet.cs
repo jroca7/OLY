64fea65906c71012dca707da8721efc69f256262
jroca7 <70997708+jroca7@users.noreply.github.com>
2021-11-23 18:04:53 -0300

    SubidaCommit

---
+++ ParcialProgramacionII/Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Bullet : MonoBehaviour
+{
+    public int scoreValue = 5;
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            //ScoreManager.score + scoreValue;
+        }
+        else
+        {
+         Destroy(gameObject);
+        }
+    }
+}
